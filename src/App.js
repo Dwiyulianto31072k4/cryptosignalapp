de87cd0345a03377a2016d07f9db3c17ae380531
Dwi Yulianto <110501342+Dwiyulianto31072k4@users.noreply.github.com>
2025-05-02 12:57:35 +0700

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CryptoSignalParser from './components/CryptoSignalParser';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoSignalParser />
+    </div>
+  );
+}
+
+export default App;
